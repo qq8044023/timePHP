e9ebb3318524a34dcf532c68b4348d950b45e4c4
qq8044023 <cqkxm@qq.com>
2017-02-13 16:21:39 +0800

    update

--- task/common.php
+++ task/common.php
@@ -3,3 +3,38 @@
  * 函数
  * @author 码农<8044023@qq.com>
  *   */
+/////////////////////////////////////////////////////////////////////////////////
+/**
+ * 邮件发送
+ * @param   string   $to     发送的邮箱地址
+ * @param   string   $name   名字
+ * @param   string   $subject  描述
+ * @param   string   $body      内容
+ * @param   array    $attachment    发送的文件附件
+ *   */
+function timePHP_send_mail($to, $name, $subject = '', $body = '', $attachment = null){
+    //初始化
+    $mail             = new \PHPMailer(); //PHPMailer对象
+    $mail->CharSet    = 'UTF-8'; //设定邮件编码，默认ISO-8859-1，如果发中文此项必须设置，否则乱码
+    $mail->IsSMTP();  // 设定使用SMTP服务
+    $mail->SMTPDebug  = 0;                     // 关闭SMTP调试功能
+    $mail->SMTPAuth   = true;                  // 启用 SMTP 验证功能
+    $mail->SMTPSecure = 'tls';                 // 使用安全协议
+    $mail->Host       = C("EMAIL")['SMTP_HOST'];  // SMTP 服务器
+    $mail->Port       = C("EMAIL")['SMTP_PORT'];  // SMTP服务器的端口号
+    $mail->Username   = C("EMAIL")['SMTP_USER'];  // SMTP服务器用户名
+    $mail->Password   = C("EMAIL")['SMTP_PASS'];  // SMTP服务器密码
+    $mail->SetFrom(C("EMAIL")['FROM_EMAIL'], C("EMAIL")['FROM_NAME']);
+    $replyEmail       = C("EMAIL")['REPLY_EMAIL']?C("EMAIL")['REPLY_EMAIL']:C("EMAIL")['FROM_EMAIL'];
+    $replyName        = C("EMAIL")['REPLY_NAME']?C("EMAIL")['REPLY_NAME']:C("EMAIL")['FROM_NAME'];
+    $mail->AddReplyTo($replyEmail, $replyName);
+    $mail->Subject    = $subject;
+    $mail->MsgHTML($body);
+    $mail->AddAddress($to, $name);
+    if(is_array($attachment)){ // 添加附件
+        foreach ($attachment as $file){
+            is_file($file) && $mail->AddAttachment($file);
+        }
+    }
+    return $mail->Send() ? true : $mail->ErrorInfo;
+}
--- task/config.php
+++ task/config.php
@@ -1,43 +1,54 @@
 <?php 
 /**
  * 任务进程中的配置文件
- * @author 码农<8044023@qq.com>
- *   */
+ * time 单位秒
+ * @var array  */
 return [
     //任务 id
     "TASK"=>[
         "clearmeesage"=>[
-            "time"=>2,//时间周期 /秒
-            "number"=>0,//序列号
-            "name"=>"clearmeesage"//进程名 （和任务名一样）
-        ],//清除短信中不要的垃圾数据 
+            "time"=>60,
+            "number"=>0,
+            "name"=>"clearmeesage"
+        ],//清除短信中不要的垃圾数据
         "clearroom"=>[
-            "time"=>2,//时间周期 /秒
-            "number"=>1,//序列号
-            "name"=>"clearroom"//进程名 （和任务名一样）
+            "time"=>3600,
+            "number"=>1,
+            "name"=>"clearroom"
         ],//清除房间中不要的垃圾数据
         "backup"=>[
-            "time"=>2,//时间周期 /秒
-            "number"=>2,//序列号
-            "name"=>"backup",//进程名 （和任务名一样）
+            "time"=>86400,//多久备份一次
+            "number"=>2,
+            "name"=>"backup",
             "target_dir"=>"/home/bak/",//备份的路径
-            "db_array"=>[//要备份的数据库
+            "dbArray"=>[//要备份的数据库
                 "tourism_game",
-                "tourism_game2"
             ],
             "past_time"=>9//过期时间/天
         ],//数据库定时备份
     ],
-    "EXECUTE"=>["clearroom","backup","clearmeesage"],//需要启动的任务
+    "EXECUTE"=>["clearroom","backup","clearmeesage"],//需要启动的任务 "backup",
     //数据库信息
     "DB"=>array(
         'DB_TYPE' => 'mysql',
-        'DB_HOST' => '127.0.0.1',
-        'DB_NAME' => 'test',
-        'DB_USER' => 'root',
-        'DB_PWD' => 'root',
+        'DB_HOST' => 'localhost',
+        'DB_NAME' => '',//数据库名称
+        'DB_USER' => '',//数据库账号
+        'DB_PWD' => '',//密码
         'DB_PORT' => '3306',
         'DB_CODE'=>'utf8'
     ),
+    //邮件发送配置
+    "EMAIL"=>[
+        'SMTP_HOST'   => 'smtp.qq.com', //SMTP服务器
+        'SMTP_PORT'   => '587', //SMTP服务器端口
+        'SMTP_USER'   => '8044023@qq.com', //SMTP服务器用户名
+        'SMTP_PASS'   => '', //SMTP服务器密码
+        'FROM_EMAIL'  => '8044023@qq.com', //发件人EMAIL
+        'FROM_NAME'   => '测试附件服务器端', //发件人名称
+        'REPLY_EMAIL' => '', //回复EMAIL（留空则为发件人EMAIL）
+        'REPLY_NAME'  => '', //回复名称（留空则为发件人名称）
+        "GET_EMAIL"   => 'cqkxm@qq.com',//接收邮箱地址
+    ],
 ];
 ?>
