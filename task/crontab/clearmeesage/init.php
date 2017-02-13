0c69235bf71b31717eec19f3406005e31406ee90
qq8044023 <cqkxm@qq.com>
2017-02-13 16:22:24 +0800

    update

--- task/crontab/clearmeesage/init.php
+++ task/crontab/clearmeesage/init.php
@@ -7,6 +7,8 @@ namespace Crontab;
 class init{
     //入口
     static public function _init(){
-        
+        $date=date("Y-m-d H:i:s",time()-300);
+        $where="status=1 AND create_date<'".$date."'";
+        M("tourism_game_message")->where($where)->save(["status"=>-1]);
     }
 }
--- task/crontab/clearroom/init.php
+++ task/crontab/clearroom/init.php
@@ -6,6 +6,7 @@ namespace Crontab;
 class init{
     //入口
     static public function _init(){
-        
+        //1 查询出当前
+             
     }
 }
