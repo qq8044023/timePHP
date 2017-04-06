8419369e984ca8e52ccaab0ae49828342e79a360
8044023@qq.com <8044023@qq.com>
2017-04-06 17:07:52 +0800

    update

--- app/clearmeesage/clearmeesageTask.php
+++ app/clearmeesage/clearmeesageTask.php
@@ -10,7 +10,7 @@ use lib\common\SystemFun;
 class clearmeesageTask extends Task{
     public function run(){
         SystemFun::import("extend@PHPMailer@PHPMailerAutoload");
-        error_log ( "测试每小时1分钟执行一次:".date("YmdHis")."----" ,  3 ,  "/home/h.log" );
+        error_log ( "测试每1小时执行一次:".date("YmdHis")."----" ,  3 ,  "/home/h.log" );
         //测试调用 该任务下的demo
         $demo=new Demo();
         $demo->demo1();
